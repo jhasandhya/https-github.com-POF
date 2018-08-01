1a03112234fda06b6686e66c9af897757d5a39b0
jhasandhya <38009562+jhasandhya@users.noreply.github.com>
2018-08-01 21:49:10 +0530

    Add files via upload

--- LandingPage.java
+++ LandingPage.java
@@ -3,21 +3,23 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+/*import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+*/
 
 public class LandingPage{
 
 	
-	public static void main(String[] args) 
+	public static void main(String args[]) throws InterruptedException
 	{
 	
-	System.setProperty("webdriver.chrome.driver","C:\\Users\\sandhya jha\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	//System.setProperty("webdriver.chrome.driver","C:\\Users\\sandhyajha\\Downloads\\chromedriver_win32\\chromedriver.exe");
 	
 	WebDriver driver=new ChromeDriver();
 	
 	String landingPageUrl= "https://www.pof.com";
-	WebDriverWait wait=new WebDriverWait(driver, 20); 
+	
 	
 	//String expectedTitle="Google";
 	
@@ -25,6 +27,12 @@ public class LandingPage{
 	driver.manage().window().maximize();
 	driver.findElement(By.id("logincontrol_username")).sendKeys("dtk600");
 	driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+	driver.findElement(By.id("logincontrol_password")).sendKeys("androidqa");
+	System.out.println("Test Passed!");
+	driver.findElement(By.id("logincontrol_submitbutton")).click();
+	Thread.sleep(5000);
+	//driver.findElement(By.xpath("//button[contains(text(),'Check Mail!')]")).click();
+	
 	
 	//String actualTitle=driver.getTitle();
 	
